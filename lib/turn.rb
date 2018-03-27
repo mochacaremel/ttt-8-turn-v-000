b5636c132aecacc79640e7fef1a70aa73fa64086
Asheley McDonald <asheleymcdonald@gmail.com>
2018-03-27 16:52:18 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -27,3 +27,12 @@ def valid_move?(board, index)
    return false
   end
 end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip index = input_to_index(input)
+  if valid_move?(board, index) == false
+    turn(board)
+  end
+  move(board, index) display_board(board)
+end
