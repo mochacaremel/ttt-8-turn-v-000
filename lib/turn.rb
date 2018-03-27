f5a71d33eabf359fc0f899ad9a222e03473b816d
Asheley McDonald <asheleymcdonald@gmail.com>
2018-03-27 16:55:43 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -18,6 +18,8 @@ input_to_index("1") # => 0
 input_to_index("6") # => 5
 input_to_index("invalid") # => -1
 
+
+
 def valid_move?(board, index)
   if index.between?(0,8) && !position_taken?(board, index)
       puts 'this is a valid move'
