7ce1f58197a8cfedf8f14593e12834a143a56956
Asheley McDonald <asheleymcdonald@gmail.com>
2018-03-27 20:09:40 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,7 +6,7 @@ def display_board(board)
    puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 def user_input 
-  get.chomp!
+  STDIN.gets.chomp!
 end
 
 board = ["","X"," "," "," "," "," "," "," "]
