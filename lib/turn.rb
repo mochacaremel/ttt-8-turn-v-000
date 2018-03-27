bf8fc5c3befd979f33ee52d31978d52011184362
Asheley McDonald <asheleymcdonald@gmail.com>
2018-03-27 16:49:38 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,3 +9,11 @@ end
   def move(board, index, character = "X")
      board[index] = character
 end
+
+def input_to_index(user_input)
+  user_input = user_input.to_i
+  user_input = user_input - 1
+end
+input_to_index("1") # => 0
+input_to_index("6") # => 5
+input_to_index("invalid") # => -1
