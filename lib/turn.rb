5735369a225c6808aad5620fe7e7cfacbe091073
Asheley McDonald <asheleymcdonald@gmail.com>
2018-03-27 16:58:08 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,7 +6,7 @@ def display_board(board)
    puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-
+board = ["","X"," "," "," "," "," "," "," "]
 
 def move(board, index, character = "X")
      board[index] = character
